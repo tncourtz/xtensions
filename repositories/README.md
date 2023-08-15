41e0f4af5cfd891ee653d3cf2aa2ff78014b040c
Tapiwa Matare <Tapiwa.Matare@Nintex.com>
2023-08-15 17:24:50 +0200

    "Scaffold-File"

---
+++ repositories/README.md
@@ -0,0 +1,5 @@
+# 
+
+
+
+Owned by group:default/guardians
---
+++ repositories/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "4.28.0"
+    }
+  }
+  backend "azurerm" {
+    key = "repositories/template-repositories/terraform.tfstate"
+  }
+}
+
+provider "github" {
+  token = var.token
+  owner = "NintexGlobal"
+}
+
+#--------------------------------------------------------------
+# Templates Repositories
+#--------------------------------------------------------------
+
+module "repository-template-hackathon-first-order" {
+  source                          = "../../modules/github-repository"
+  repository_name                 = ""
+  description                     = "[Owner: group:default/guardians] "
+  topics                          = ""
+  branches                        = ""
+  default_branch_name             = ""
+  required_status_check_names     = ""
+  branch_protection_bypasss_users = ""
+  labels                          = ""
+}
